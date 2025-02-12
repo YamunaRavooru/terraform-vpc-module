691a796a7826c74b02d30d71949b277a3d6e52b7
yamuna <malini.mahathi@gmail.com>
2025-02-12 20:22:56 +0530

    adding code

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output rout_table_info{
 output vpc_id{
     value =aws_vpc.main.id
 }
-output public_subnet_id{
+output public_subnet_ids{
     value =aws_subnet.public[*].id
 }
-output private_subnet_id{
+output private_subnet_ids{
     value =aws_subnet.private[*].id
 }
-output database_subnet_id{
+output database_subnet_ids{
     value =aws_subnet.database[*].id
 }
